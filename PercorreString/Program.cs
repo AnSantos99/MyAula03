cc645f2408d600b30540dc8916daced7a8a9ef68
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-15 12:44:53 +0000

    Percorrer Strings

---
+++ PercorreString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaração de variaveis
+            string s, cStr;
+            char c;
+
+            //pedir string
+            Console.WriteLine("Input String");
+            s = Console.ReadLine();
+
+            //pedir char
+            Console.WriteLine("Input Char");
+            cStr = Console.ReadLine();
+
+            //converção de string para char
+            c = Convert.ToChar(cStr);
+
+            //ciclo para percorrer a string
+            foreach (char cInString in s)
+            {
+                if (cInString != c)
+                {
+                    Console.Write(cInString);
+                }
+            }
+        }
+    }
+}
